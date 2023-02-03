697dcdcfae263df9305c35a14d59e76719d90481
daus2936 <55316038+daus2936@users.noreply.github.com>
2023-02-03 20:06:56 +0700

    Update ExtentReporterNG.java

--- src/main/java/practice/resources/ExtentReporterNG.java
+++ src/main/java/practice/resources/ExtentReporterNG.java
@@ -6,7 +6,7 @@ import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 public class ExtentReporterNG {
 	
 	public static ExtentReports getReportObject() {
-		String path = System.getProperty("user.dir")+"\\report\\index.html";
+		String path = System.getProperty("user.dir")+"/report/index.html";
 		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
 		reporter.config().setReportName("Web Automation Result");
 		reporter.config().setDocumentTitle("Test Result");
